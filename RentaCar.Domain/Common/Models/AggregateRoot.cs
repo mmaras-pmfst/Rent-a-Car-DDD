e0f4fec0b1b9d2e9973fa71e16b8dcaf07a6b814
mmaras-pmfst <mmaras@pmfst.hr>
2023-01-28 11:57:21 +0100

    Created folder Models with classes ValueObject, Entity, AggregateRoot

---
+++ RentaCar.Domain/Common/Models/AggregateRoot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentaCar.Domain.Common.Models
+{
+    // AggregateRoot is also an Entity. It's sort of wrapper around Entity.
+    public abstract class AggregateRoot<TId> : Entity<TId>
+        where TId : notnull
+    {
+        protected AggregateRoot(TId id) : base(id)
+        {
+
+        }
+    }
+}
